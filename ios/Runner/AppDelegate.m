9689a5d7ef73b15e46037d79c7a8b45c1f2eddb8
Joe <joeyoung@guojiang.tv>
2025-03-17 21:13:26 +0800

    1.0.0+3

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,10 +5,6 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSArray *arr = @[@"CurrentVC", @"GeneratedPluginRegistrant"];
-    NSMutableArray *mutableArr = (NSMutableArray *)arr;
-    [mutableArr addObject:@"info"];
-
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
